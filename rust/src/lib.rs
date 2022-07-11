3e32fadea396e79957b88b6cde98ddb30688e9da
Kenny477 <53405834+Kenny477@users.noreply.github.com>
2022-07-11 14:32:43 -0400

    Working site!

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -1,17 +1,51 @@
 // This example uses 3 crates serde_json, reqwest, tokio
 
 use reqwest::Client;
-use serde_json::json;
+use serde_json::{json, Value};
 use wasm_bindgen::prelude::*;
 
 // Query to use in request
-const TRENDING: &str = "
-query { 
-  Page (page: 1, perPage: 10){
+// const TRENDING: &str = "
+// query { 
+//   Page (page: 1, perPage: 10){
+//     pageInfo {
+//       total
+//     }
+//     mediaTrends(sort: POPULARITY_DESC) {
+//       media {
+//         id
+//         title {
+//           english
+//           romaji
+//           native
+//         }
+//         description
+//         status
+//         coverImage {
+//           extraLarge
+//           large
+//           medium
+//           color
+//         }
+//       }
+//     }
+//   }
+// }
+// ";
+
+const AIRING: &str = "
+query {
+  Page {
     pageInfo {
+      perPage
       total
     }
-    mediaTrends(sort: POPULARITY_DESC) {
+    airingSchedules(notYetAired: true) {
+      id
+      airingAt
+      timeUntilAiring
+      episode
+      mediaId
       media {
         id
         title {
@@ -33,16 +67,12 @@ query {
 }
 ";
 
-pub fn main() {
-  poll(query());
-}
-
 // #[tokio::main]
 #[wasm_bindgen]
-pub async fn query() {
+pub async fn query() -> JsValue {
     let client = Client::new();
     // Define query and variables
-    let json = json!({"query": TRENDING});
+    let json = json!({"query": AIRING});
     // Make HTTP post request
     let resp = client
         .post("https://graphql.anilist.co/")
@@ -56,5 +86,6 @@ pub async fn query() {
         .await;
     // Get json
     let result: serde_json::Value = serde_json::from_str(&resp.unwrap()).unwrap();
-    println!("{:#?}", result);
+    // println!("{:#?}", result);
+    return JsValue::from_serde(&result).unwrap();
 }
--- svelte/src/App.svelte
+++ svelte/src/App.svelte
@@ -1,26 +1,51 @@
 <script>
     export let bindings;
 
-    console.log('script')
+    let res = bindings.query();
 
-    let data;
-    console.log(typeof bindings.query)
-    let query = bindings.query();
-    query.then(res => {
-        console.log(res)
-    })
-    console.log(query)
-    bindings.query().then((res) => {
-        console.log(res);
-        data = res;
-    });
+    function chooseTitle(title) {
+        return title.romanji
+            ? title.romanji
+            : title.english
+            ? title.english
+            : title.native
+            ? title.native
+            : "No title found";
+    }
 </script>
 
-<main>
+<main class="h-screen">
     <div class="navbar">
         <div>
             <a class="btn btn-ghost" href="/">anitrends</a>
-            {data}
         </div>
+    </div>
+    <div class="carousel space-x-4 rounded-box px-10">
+        {#await res}
+            <div class="radial-progress animate-spin" style="--value:70;" />
+        {:then res}
+            {#each res.data.Page.airingSchedules as item}
+                <div class="carousel-item w-52">
+                    <div class="card image-full bg-base-100 shadow-xl wrap">
+                        <figure>
+                            <img
+                                src={item.media.coverImage.extraLarge}
+                                alt={chooseTitle(item.media.title)}
+                            />
+                        </figure>
+                        <div class="card-body">
+                            <h1 class="card-title text-md">
+                                {chooseTitle(item.media.title)}
+                            </h1>
+                            <!-- <p class="text-sm truncate">
+                                {@html item.media.description
+                                    ? item.media.description
+                                    : ""}
+                            </p> -->
+                        </div>
+                    </div>
+                </div>
+            {/each}
+        {/await}
     </div>
 </main>
